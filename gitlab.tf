dd358e96216fba4e5da53deb96a3eb743e1b5257
Ben Ballard <32252244+wbaballard@users.noreply.github.com>
2023-04-21 03:06:01 -0400

    Update gitlab.tf

--- gitlab.tf
+++ gitlab.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "gl-server-instance" {
   instance_type     = "t2.micro"
   availability_zone = "us-east-1a"
   # Insert your generated EC2 key pair information from AWS
-  key_name          = "main-key"
+  key_name          = "{your_key}"
 
     network_interface {
     device_index         = 0
